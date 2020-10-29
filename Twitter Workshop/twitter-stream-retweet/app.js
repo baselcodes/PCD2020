a13db9c9f5d3355a4c5f8667ed61588ba2ec6842
Max Frischknecht <max@debutdebut.com>
2020-10-29 19:25:16 +0100

    update retweet script

--- Twitter Workshop/twitter-stream-retweet/app.js
+++ Twitter Workshop/twitter-stream-retweet/app.js
@@ -8,8 +8,8 @@ let config = {
   consumer_secret: process.env.SECRET,
   access_token: process.env.TOKEN,
   access_token_secret: process.env.TOKENSECRET,
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL:            true     // optional - requires SSL certificates to be valid.
+  timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL: true     // optional - requires SSL certificates to be valid.
 }
 
 // import twit library
@@ -20,11 +20,12 @@ var T = new Twit(config);
 
 // If you want to stream some hashtags
 let trackTags = {
-  track: '#USElection'
+  track: '#election'
 }
 
 // If you want to stream users
 // You need the user-twitter-id, get it here: http://gettwitterid.com/
+// this also includes tweets that mention these profiles.
 let trackUsers = {
   // shiffman, processing, casey reas, p5js, guardian, wired
   follow: "14587429, 876624356, 3422140683, 2749534722, 87818409, 1344951"
@@ -38,32 +39,32 @@ let stream = T.stream('statuses/filter', trackTags)
 stream.on('tweet', streamEvent);
 
 // Callback function: get the tweet and make a json out of it
-function streamEvent(data){
-    const element = {};
-  
-    element.id = data.id_str;
-    element.text = data.text;
-    element.user_name = data.user.name;
-    element.created_at = data.created_at;
-    // log the element
-    console.log(element);
-    
-    // OPTIONAL: Influence the public opinion and repost the tweet ;)
-    // retweetData(element); 
+function streamEvent(data) {
+  const element = {};
+
+  element.id = data.id_str;
+  element.text = data.text;
+  element.user_name = data.user.name;
+  element.created_at = data.created_at;
+  // log the data
+  console.log(element);
+
+  // OPTIONAL: Influence the public opinion and repost the tweet ;)
+  // retweetData(element); 
 }
 
 // Get the id from the json and retweet the thing!
-function retweetData(data){
+function retweetData(data) {
   let tweetId = {
     id: data.id
   }
   T.post('statuses/retweet/:id', tweetId)
-  .catch(function (err) {
-    console.log('Error yo!', err.stack)
-  })
-  .then(function (result) {
-    console.log("Dude, it was retweeted!");
-  });
+    .catch(function (err) {
+      console.log('Error yo!', err.stack)
+    })
+    .then(function (result) {
+      console.log("Dude, it was retweeted!");
+    });
 }
 
 
